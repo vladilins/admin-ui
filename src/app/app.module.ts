6717195ada6453555b642966861c16aed27df465
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-07-05 17:29:47 +0300

    input style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { FormComponent } from './components/form/form.component';
-import { AdsComponent } from './components/ads/ads.component';
+import { AppComponent } from "./app.component";
+import { FormComponent } from "./components/form/form.component";
+import { AdsComponent } from "./components/ads/ads.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    AdsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, FormComponent, AdsComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+  ChangeDetectionStrategy,
+  OnInit
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormArray,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
+  exists = false;
 
-  constructor() { }
+  // @Input() pizza: Pizza;
+  // @Input() toppings: Topping[];
 
-  ngOnInit() {
+  // @Output() selected = new EventEmitter<Pizza>();
+  // @Output() create = new EventEmitter<Pizza>();
+  // @Output() update = new EventEmitter<Pizza>();
+  // @Output() remove = new EventEmitter<Pizza>();
+
+  form = this.fb.group({
+    name: ["", Validators.required],
+    toppings: [[]]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  get nameControl() {
+    return this.form.get("name") as FormControl;
   }
 
+  get nameControlInvalid() {
+    return this.nameControl.hasError("required") && this.nameControl.touched;
+  }
+
+  // ngOnChanges(changes: SimpleChanges) {
+  //   if (this.pizza && this.pizza.id) {
+  //     this.exists = true;
+  //     this.form.patchValue(this.pizza);
+  //   }
+  //   this.form
+  //     .get("toppings")
+  //     .valueChanges.pipe(
+  //       map(toppings => toppings.map((topping: Topping) => topping.id))
+  //     )
+  //     .subscribe(value => this.selected.emit(value));
+  // }
+
+  // createPizza(form: FormGroup) {
+  //   const { value, valid } = form;
+  //   if (valid) {
+  //     this.create.emit(value);
+  //   }
+  // }
+
+  // updatePizza(form: FormGroup) {
+  //   const { value, valid, touched } = form;
+  //   if (touched && valid) {
+  //     this.update.emit({ ...this.pizza, ...value });
+  //   }
+  // }
+
+  // removePizza(form: FormGroup) {
+  //   const { value } = form;
+  //   this.remove.emit({ ...this.pizza, ...value });
+  // }
 }
