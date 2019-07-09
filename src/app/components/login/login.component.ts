5338f2069f0390454e187956fcf10ce9c851e08f
Vladislav Ilin <vladislav.ilin@hiq.ru>
2019-07-09 13:34:52 +0300

    login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  submitted = false;
 
-  constructor() { }
+  form = this.fb.group({
+    username: ["", Validators.required],
+    password: ["", Validators.required]
+  });
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  get usernameControl() {
+    return this.form.get("username") as FormControl;
   }
 
+  get passwordControl() {
+    return this.form.get("password") as FormControl;
+  }
+
+  login(form: FormGroup) {
+    this.submitted = true;
+    const { value, valid } = form;
+    if (valid) {
+      console.log(form.value);
+    }
+  }
 }
